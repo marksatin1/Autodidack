8a6488b6c5b23c0382a4d583e253279509ee0876
Mark Satin <msatin2009@gmail.com>
2024-02-19 21:46:18 -0500

    general updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import BackgroundCarousel from "./ui/bg-carousel";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="h-screen overflow-hidden px-12 py-8">
+        <BackgroundCarousel />
+      </div>
+    </main>
+  );
 }
