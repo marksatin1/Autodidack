0d91b82dc69728fead40f9c7ac65272cc2c67d77
Mark Satin <msatin2009@gmail.com>
2024-02-19 21:43:23 -0500

    apply general main/footer layout to whole app; still needs work

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "./ui/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +11,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} h-screen overflow-hidden`}>
+        <div>{children}</div>
+        <Footer />
+      </body>
     </html>
   );
 }
