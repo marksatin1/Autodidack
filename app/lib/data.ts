ced5e0bc8a153b3ae1eeed90d2a0e4b3912de27b
Mark Satin <msatin2009@gmail.com>
2024-02-19 21:45:00 -0500

    update to data shapes, add bg-gallery data/defs

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,4 @@
-import { NavLink } from "./definitions";
+import { Gallery, NavLink, Photo } from "./definitions";
 
 export const navLinks: NavLink[] = [
   {
@@ -6,4 +6,169 @@ export const navLinks: NavLink[] = [
     href: "/galleries",
   },
   { name: "About", href: "/about" },
+];
+
+export const slides: Photo[] = [
+  {
+    id: 1,
+    url: "/1.jpg",
+    width: 500,
+    height: 500,
+    description: "Test",
+  },
+  {
+    id: 2,
+    url: "/2.jpg",
+    width: 500,
+    height: 500,
+    description: "Test",
+  },
+  {
+    id: 3,
+    url: "/3.jpg",
+    width: 500,
+    height: 500,
+    description: "Test",
+  },
+  {
+    id: 4,
+    url: "/4.jpg",
+    width: 500,
+    height: 500,
+    description: "Test",
+  },
+  {
+    id: 5,
+    url: "/5.jpg",
+    width: 500,
+    height: 500,
+    description: "Test",
+  },
+];
+
+export const galleries: Gallery[] = [
+  {
+    id: 1,
+    name: "black_and_white",
+    path: "/galleries/black_and_white",
+    photo: {
+      id: "",
+      url: "/1.jpg",
+      width: 500,
+      height: 500,
+      description: "",
+    },
+  },
+  {
+    id: 2,
+    name: "george_floyd_2020",
+    path: "/galleries/george_floyd_2020",
+    photo: {
+      id: "",
+      url: "/2.jpg",
+      width: 500,
+      height: 500,
+      description: "",
+    },
+  },
+  {
+    id: 3,
+    name: "landscapes",
+    path: "/galleries/landscapes",
+    photo: {
+      id: "",
+      url: "/3.jpg",
+      width: 500,
+      height: 500,
+      description: "",
+    },
+  },
+  {
+    id: 4,
+    name: "cityscapes",
+    path: "/galleries/cityscapes",
+    photo: {
+      id: "",
+      url: "/4.jpg",
+      width: 500,
+      height: 500,
+      description: "",
+    },
+  },
+  {
+    id: 5,
+    name: "street",
+    path: "/galleries/street",
+    photo: {
+      id: "",
+      url: "/5.jpg",
+      width: 500,
+      height: 500,
+      description: "",
+    },
+  },
+  {
+    id: 6,
+    name: "portraits",
+    path: "/galleries/portraits",
+    photo: {
+      id: "",
+      url: "/Cowboy-Bebop-Wallpaper.jpg",
+      width: 500,
+      height: 500,
+      description: "",
+    },
+  },
+];
+
+export const bgPhotos: Photo[] = [
+  {
+    id: 1,
+    url: "/aut-bg-1.avif",
+    width: 2000,
+    height: 1639,
+    description: "Test",
+  },
+  {
+    id: 2,
+    url: "/aut-bg-2.avif",
+    width: 2000,
+    height: 1643,
+    description: "Test",
+  },
+  {
+    id: 3,
+    url: "/aut-bg-3.avif",
+    width: 2000,
+    height: 1639,
+    description: "Test",
+  },
+  {
+    id: 4,
+    url: "/aut-bg-4.avif",
+    width: 2000,
+    height: 1633,
+    description: "Test",
+  },
+  {
+    id: 5,
+    url: "/aut-bg-5.avif",
+    width: 2000,
+    height: 1638,
+    description: "Test",
+  },
+  {
+    id: 6,
+    url: "/aut-bg-6.avif",
+    width: 2000,
+    height: 1637,
+    description: "Test",
+  },
+  {
+    id: 7,
+    url: "/aut-bg-7.avif",
+    width: 200,
+    height: 1639,
+    description: "Test",
+  },
 ];
--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,3 +2,18 @@ export type NavLink = {
   name: string;
   href: string;
 };
+
+export type Photo = {
+  id: number | string;
+  url: string;
+  width: number;
+  height: number;
+  description: string;
+};
+
+export type Gallery = {
+  id: number | string;
+  name: string;
+  path: string;
+  photo: Photo;
+};
