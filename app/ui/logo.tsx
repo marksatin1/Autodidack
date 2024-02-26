c6f7f19933ab9996b6bc1684e8d326d7882ca3b1
Mark Satin <msatin2009@gmail.com>
2024-02-26 13:16:22 -0500

    moved fontSize prop to style; span to p tag

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,11 +1,17 @@
 import { Inconsolata } from "next/font/google";
+import Link from "next/link";
 
 const inconsolata = Inconsolata({ subsets: ["latin"] });
 
 export default function Logo({ fontSize }: { fontSize: string }) {
   return (
-    <span className={`${inconsolata.className} text-[${fontSize}] text-white logo-effect unblur hover:text-amber-500`}>
-      Autodidack
-    </span>
+    <Link href="/">
+      <p
+        className={`${inconsolata.className} text-white logo-effect unblur hover:text-amber-500`}
+        style={{ fontSize }}
+      >
+        Autodidack
+      </p>
+    </Link>
   );
 }
