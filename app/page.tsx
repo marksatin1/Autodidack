bf130ef2f9d32ba033d3a436d8b96d90402d1de3
Mark Satin <msatin2009@gmail.com>
2024-02-25 19:52:36 -0500

    style change for bg-carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 import BackgroundCarousel from "./ui/bg-carousel";
 import { getHomePhotos } from "./lib/actions";
+import Image from "next/image";
+import Logo from "./ui/logo";
 
 export default async function Home() {
   const homePhotos = await getHomePhotos();
 
   return (
-    <main>
-      <div className="h-screen overflow-hidden px-1 py-2">
-        {homePhotos && <BackgroundCarousel images={homePhotos} />}
+    <main className="relative h-screen overflow-hidden p-2">
+      {/* <div className="absolute top-[10%] left-1/2 transform -translate-x-1/2">
+        <Logo size="9xl" />
+      </div> */}
+      {homePhotos && <BackgroundCarousel images={homePhotos} />}
+      <div className="absolute top-10 bottom-0 left-0 right-0 mx-auto w-3/4 h-5/6">
+        <Image
+          src="/autodidack-collage.webp"
+          width={3583}
+          height={2395}
+          alt="Collage of people from street style photography"
+          className="object-contain w-full h-full"
+        />
       </div>
     </main>
   );
