d60206b919011304a7bf431a1a4cffd6c404209d
Mark Satin <msatin2009@gmail.com>
2024-02-26 13:13:57 -0500

    remove logo from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 import BackgroundCarousel from "./ui/bg-carousel";
 import { getHomePhotos } from "./lib/actions";
 import Image from "next/image";
-import Logo from "./ui/logo";
 
 export default async function Home() {
   const homePhotos = await getHomePhotos();
 
   return (
     <main className="relative h-screen overflow-hidden p-2">
-      {/* <div className="absolute top-[10%] left-1/2 transform -translate-x-1/2">
-        <Logo size="9xl" />
-      </div> */}
       {homePhotos && <BackgroundCarousel images={homePhotos} />}
       <div className="absolute top-10 bottom-0 left-0 right-0 mx-auto w-3/4 h-5/6">
         <Image
