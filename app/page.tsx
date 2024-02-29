23d8de0ba33f4a95dcd292df3680d65abf22fa7e
Mark Satin <msatin2009@gmail.com>
2024-02-29 14:11:12 -0500

    update collage image; add appear animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import BackgroundCarousel from "./ui/bg-carousel";
 import { getHomePhotos } from "./lib/actions";
 import Image from "next/image";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faMusic, faDrumSteelpan, faMicrophone, faMicrophoneSlash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMusic,
+  faDrumSteelpan,
+  faMicrophone,
+  faMicrophoneSlash,
+} from "@fortawesome/free-solid-svg-icons";
 
 library.add(faMusic, faDrumSteelpan, faMicrophone, faMicrophoneSlash);
 
@@ -14,11 +19,11 @@ export default async function Home() {
       {homePhotos && <BackgroundCarousel images={homePhotos} />}
       <div className="absolute top-10 bottom-0 left-0 right-0 mx-auto w-3/4 h-5/6">
         <Image
-          src="/autodidack-collage.webp"
+          src="/Autodidack-Collage_stroke.webp"
           width={3583}
           height={2395}
           alt="Collage of people from street style photography"
-          className="object-contain w-full h-full"
+          className="object-contain w-full h-full appear"
         />
       </div>
     </main>
