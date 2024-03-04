a35aacf7022e38f0c805ec3bb2bc48810db228d8
Mark Satin <msatin2009@gmail.com>
2024-03-04 15:39:11 -0500

    add two column layout for lg screens and up

--- app/galleries/page.tsx
+++ app/galleries/page.tsx
@@ -5,8 +5,6 @@ import Link from "next/link";
 import { getGalleriesPagePhotos } from "../lib/actions";
 import { Gallery } from "../lib/definitions";
 import { useState, useEffect } from "react";
-import { Suspense } from "react";
-import { GalleriesSkeleton } from "@/app/ui/skeletons";
 
 export default function Page() {
   const [galleries, setGalleries] = useState<Gallery[]>([]);
@@ -19,21 +17,21 @@ export default function Page() {
   }, []);
 
   return (
-    <div className="grid grid-cols-2 gap-8 p-4 overflow-scroll">
-      {galleries.map((g: Gallery) => {
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 p-4 w-full h-full overflow-scroll">
+      {galleries.map((g: Gallery, i: number) => {
         return (
-          <div key={g.id} className="grid justify-center">
+          <div key={g.id} className="grid w-full h-full">
             <Link href={`/galleries/${g.id}/${g.name}`}>
               <Image
                 src={g.cover_url}
                 width={g.cover_width_px}
                 height={g.cover_height_px}
                 alt={g.description}
-                className="shadow-[-3px_5px_10px_black]"
-                priority
+                className="max-w-[90%] mx-auto shadow-[-3px_5px_10px_black]"
+                priority={i === 0}
               />
             </Link>
-            <h2 className="text-2xl text-center font-extrabold my-4">{g.name}</h2>
+            <h2 className="text-2xl text-center font-extrabold mt-4 mb-12">{g.name}</h2>
           </div>
         );
       })}
