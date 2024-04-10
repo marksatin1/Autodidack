3ec609aff1551417de4ef462d1e4a959179d368d
Mark Satin <msatin2009@gmail.com>
2024-04-10 13:40:50 -0400

    import navbars; update styling to figma spec

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,28 +1,29 @@
+import { externalNavLinks, internalNavLinks } from "../lib/data";
+import ExternalNavbar from "./external-navbar";
+import InternalNavbar from "./internal-navbar";
+import Image from "next/image";
+
 export default function Footer() {
   return (
-    <footer className="bsr flex justify-center items-center">
-      <nav>
-        <ul>
-          <li>Web</li>
-          <li>Photo</li>
-          <li>About</li>
-        </ul>
-      </nav>
-      <hr />
-      <div>
-        <nav>
-          <ul>
-            <li>In</li>
-            <li>GH</li>
-            <li>LI</li>
-            <li>UW</li>
-          </ul>
-        </nav>
-        <div>
+    <footer className="flex flex-col justify-center items-center space-y-3 px-8 py-4">
+      <InternalNavbar links={internalNavLinks} />
+      <hr className="w-1/4 border-slate-500 " />
+      <div className="w-full grid grid-cols-3">
+        <div className="justify-self-start">
+          <ExternalNavbar links={externalNavLinks} />
+        </div>
+        <div className="flex flex-col justify-center items-center space-y-0.5 text-slate-500">
           <small>Designed by Mark Satin</small>
           <small>&copy; 2024</small>
         </div>
-        <div>Sound Icon</div>
+        <div className="justify-self-end">
+          <Image
+            src="/icons/2x/128/Listening.png"
+            alt="Black and white icon of a person listening to noise signaling sound is on"
+            width={32}
+            height={32}
+          />
+        </div>
       </div>
     </footer>
   );
