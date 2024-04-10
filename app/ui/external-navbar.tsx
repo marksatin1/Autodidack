c54f07c300c42d1c40fff61e98670a794948a9ea
Mark Satin <msatin2009@gmail.com>
2024-04-10 13:08:07 -0400

    init commit

---
+++ app/ui/external-navbar.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { NavLink } from "../lib/definitions";
+
+export default function ExternalNavbar({ links }: { links: NavLink[] }) {
+  return (
+    <nav>
+      <ul className="flex items-center justify-center space-x-8">
+        {links.map(l => {
+          return (
+            <li key={l.id}>
+              <a href={l.href} target="_blank" rel="noopener noreferrer">
+                <Image
+                  src={l.image!.path}
+                  alt={l.image!.description}
+                  width={l.image!.width}
+                  height={l.image!.height}
+                />
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
