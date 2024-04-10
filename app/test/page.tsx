3d25ad234df150d3d35257a4c226ea76ecf10050
Mark Satin <msatin2009@gmail.com>
2024-04-09 23:28:57 -0400

    init commit

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,10 @@
-import { CollageImage } from "../lib/definitions";
-import { collageImages } from "../lib/data";
+// import { CollageImage } from "../lib/definitions";
+// import { collageImages } from "../lib/data";
 
 export default function Test() {
   return (
     <div className="w-1/2 h-auto m-auto">
-      <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1283">
+      {/* <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1283">
         {collageImages.map((img: CollageImage) => {
           return (
             <image
@@ -18,7 +18,7 @@ export default function Test() {
             />
           );
         })}
-      </svg>
+      </svg> */}
     </div>
   );
 }
