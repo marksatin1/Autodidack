559bc8b673ba33a143f1c151875746c80cee8620
Mark Satin <msatin2009@gmail.com>
2024-04-10 12:27:35 -0400

    init commit

---
+++ app/ui/footer.tsx
@@ -0,0 +1,29 @@
+export default function Footer() {
+  return (
+    <footer className="bsr flex justify-center items-center">
+      <nav>
+        <ul>
+          <li>Web</li>
+          <li>Photo</li>
+          <li>About</li>
+        </ul>
+      </nav>
+      <hr />
+      <div>
+        <nav>
+          <ul>
+            <li>In</li>
+            <li>GH</li>
+            <li>LI</li>
+            <li>UW</li>
+          </ul>
+        </nav>
+        <div>
+          <small>Designed by Mark Satin</small>
+          <small>&copy; 2024</small>
+        </div>
+        <div>Sound Icon</div>
+      </div>
+    </footer>
+  );
+}
