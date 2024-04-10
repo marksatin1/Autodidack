d953c6638f8d6f87e99c5520c5599328f88b3fda
Mark Satin <msatin2009@gmail.com>
2024-04-10 12:29:00 -0400

    init commit

---
+++ app/ui/internal-navbar.tsx
@@ -0,0 +1,20 @@
+import { InternalNavLink } from "../lib/definitions";
+import Link from "next/link";
+
+export default function InternalNavbar({ links }: { links: InternalNavLink[] }) {
+  return (
+    <nav>
+      <ul className="flex items-center justify-center space-x-32">
+        {links.map(l => {
+          return (
+            <li key={l.id}>
+              <Link href={l.href} className="text-5xl drop-shadow-md">
+                {l.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
