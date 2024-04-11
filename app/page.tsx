4b551d3dda7861855b1b28c4f37cc9aaac0e1eba
Mark Satin <msatin2009@gmail.com>
2024-04-10 20:14:14 -0400

    major style cleanup, positioning organization

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,18 @@
-import BackgroundCarousel from "./ui/bg-carousel";
+import AutoCarousel from "./ui/auto-carousel";
 import { getHomePhotos } from "./lib/actions";
-import Image from "next/image";
-import Logo from "./ui/logo";
-import NavBar from "./ui/nav-bar";
+import Collage from "./ui/collage";
+import { collageImages } from "./lib/data";
 
 export default async function Home() {
   const homePhotos = await getHomePhotos();
 
   return (
-    <main className="relative w-screen h-screen">
-      <div className="absolute top-0 left-0 z-0 w-full h-full p-1">
-        {homePhotos && <BackgroundCarousel images={homePhotos} />}
+    <section className="h-full">
+      {homePhotos && <AutoCarousel images={homePhotos} />}
+
+      <div className="appear-in h-full flex justify-center items-center">
+        <Collage images={collageImages} />
       </div>
-
-      <div className="grid grid-rows-fixed-ends w-full h-full absolute top-0 left-0 z-10 overflow-hidden lg:grid-rows-equal">
-        <div className="grid justify-center items-end lg:items-center lg:mt-8 appear-out">
-          <Logo />
-        </div>
-
-        <div className="w-full h-full p-4">
-          <div className="w-full h-full flex justify-center items-center appear-in">
-            <Image
-              src="/Autodidack-Collage_stroke.webp"
-              width={2500}
-              height={1671}
-              alt="Collage of people taken from street style photographs"
-              className="w-full h-full object-contain sm:max-w-[90%] md:max-w-[80%] lg:max-w-[80%] xl:max-w-[65%]"
-            />
-          </div>
-        </div>
-
-        <footer className="w-full appear-out">
-          <NavBar />
-        </footer>
-      </div>
-    </main>
+    </section>
   );
 }
