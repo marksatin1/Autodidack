4c0bc4bd50748b554506b6646b58892b07a9958c
Mark Satin <msatin2009@gmail.com>
2024-04-11 12:49:38 -0400

    add custom box shadow props

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,12 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        gallerySm: "-3px 5px 10px rgba(0, 0, 0, 0.5)",
+        galleryMd: "-5px 7px 2px rgba(0, 0, 0, 1)",
+      },
+    },
   },
   plugins: [],
 };
