1fa7fd18bc801ac204fa047667a874ed9eb44889
Mark Satin <msatin2009@gmail.com>
2024-04-12 19:59:48 -0400

    testing photo enlarging animation

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,35 @@
-import { collageImages } from "../lib/data";
-import Collage from "../ui/collage";
+"use client";
+
+import Image from "next/image";
+import { useContext, useState } from "react";
+import { AnimationContext } from "../lib/context/AnimationContext";
 
 export default function Test() {
+  const [isPhotoEnlarged, setIsPhotoEnlarged] = useState<boolean>(false);
+  const { setImageGrowth } = useContext(AnimationContext);
+
+  function handlePhotoClick() {
+    setImageGrowth(true);
+    setIsPhotoEnlarged(prev => !prev);
+  }
+
   return (
-    <>
-      <Collage images={collageImages} />
-      {/* <div className="grid justify-center items-end lg:items-center lg:mt-8 appear-out">
-          <Logo />
-        </div> */}
-    </>
+    <div className="grid grid-rows-[auto_1fr_auto] h-screen">
+      <div className="bg-slate-600">Header</div>
+      <div>
+        <Image
+          src={"/Profile-Pic_Square.jpeg"}
+          alt="Profile Pic"
+          width={1367}
+          height={1367}
+          onClick={handlePhotoClick}
+          className={`w-full h-auto m-auto ${
+            isPhotoEnlarged ? "scale-[12] duration-[2000ms] inset-0 z-50 overflow-visible" : ""
+          }`}
+          style={{ width: 400, height: 400 }}
+        />
+      </div>
+      <div className="bg-slate-600">Footer</div>
+    </div>
   );
 }
