26fafbf7c710b4e76475449f98881d052427123c
Mark Satin <msatin2009@gmail.com>
2024-04-16 10:34:22 -0400

    replace all fonts with Merriweather

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import "./globals.css";
 import Footer from "./ui/layout/footer";
 import Header from "./ui/layout/header";
-import { Kreon } from "next/font/google";
+import { Lora, Merriweather } from "next/font/google";
 
-const kreon = Kreon({
+const lora = Lora({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const merri = Merriweather({
   subsets: ["latin"],
   weight: "400",
 });
@@ -14,10 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={kreon.className}>
-      <body className="h-screen grid grid-rows-[auto_1fr_auto]">
+    <html lang="en" className={merri.className}>
+      <body className="w-screen h-screen grid grid-rows-[auto_1fr_auto]">
         <Header />
-        <main className="overflow-hidden">{children}</main>
+        <main className="w-full max-h-[750px]">{children}</main>
         <Footer />
       </body>
     </html>
--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,13 +1,8 @@
-import { Inconsolata } from "next/font/google";
 import Link from "next/link";
-
-const inconsolata = Inconsolata({ subsets: ["latin"] });
 
 export default function Logo() {
   return (
-    <h1
-      className={`${inconsolata.className} text-white text-4xl trackLetters logo-effect unblur hover:text-amber-500`}
-    >
+    <h1 className="text-white text-4xl trackLetters logo-effect unblur hover:text-amber-500">
       <Link href="/">Autodidack</Link>
     </h1>
   );
