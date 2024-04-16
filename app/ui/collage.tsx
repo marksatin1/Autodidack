a1fc25ece9cb247c86122b3194ecafbee8be5d0f
Mark Satin <msatin2009@gmail.com>
2024-04-16 10:36:53 -0400

    remove <section>

--- app/ui/collage.tsx
+++ app/ui/collage.tsx
@@ -2,21 +2,23 @@ import { CollageImage } from "../lib/definitions";
 
 export default function Collage({ images }: { images: CollageImage[] }) {
   return (
-    <section className="w-[60%]">
-      <svg className="w-full h-full" viewBox="0 0 2700 1900" xmlns="http://www.w3.org/2000/svg">
-        {images.map((i: CollageImage) => {
-          return (
-            <image
-              key={i.id}
-              href={i.path}
-              x={i.xCoord}
-              y={i.yCoord}
-              width={i.width}
-              height={i.height}
-            />
-          );
-        })}
-      </svg>
-    </section>
+    <svg
+      viewBox="0 0 2700 1900"
+      xmlns="http://www.w3.org/2000/svg"
+      className="object-contain w-3/4 h-full m-auto"
+    >
+      {images.map((i: CollageImage) => {
+        return (
+          <image
+            key={i.id}
+            href={i.path}
+            x={i.xCoord}
+            y={i.yCoord}
+            width={i.width}
+            height={i.height}
+          />
+        );
+      })}
+    </svg>
   );
 }
