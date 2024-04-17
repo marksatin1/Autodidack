c013291d6ed3d396bce5d5e2ca37b8b0ce10c827
Mark Satin <msatin2009@gmail.com>
2024-04-17 19:07:54 -0400

    remove old nonexistent API call

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,6 @@
-import AutoCarousel from "../ui/auto-carousel";
-import { getHomePhotos } from "../lib/actions";
 import LowerDiv from "../ui/lower-div";
 
 export default async function Page() {
-  const homePhotos = await getHomePhotos();
 
   return (
     <div className="bsr w-full h-full overflow-hidden">
