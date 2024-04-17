059b743e4a7963366699a6c3ecdd1acb1de8af38
Mark Satin <msatin2009@gmail.com>
2024-04-17 10:21:55 -0400

    move all sound state funcionality to new AudioPlayer component

--- app/ui/layout/footer.tsx
+++ app/ui/layout/footer.tsx
@@ -1,20 +1,9 @@
-"use client";
-
 import { externalNavLinks, internalNavLinks } from "../../lib/data";
+import AudioPlayer from "./audio-player";
 import ExternalNavbar from "./external-navbar";
 import InternalNavbar from "./internal-navbar";
-import Image from "next/image";
-import { useState } from "react";
 
 export default function Footer() {
-  const [audioIconPath, setAudioIconPath] = useState<string>("/icons/2x/128/Quiet.png");
-
-  function updateAudioIcon() {
-    audioIconPath === "/icons/2x/128/Quiet.png"
-      ? setAudioIconPath("/icons/2x/128/Listening.png")
-      : setAudioIconPath("/icons/2x/128/Quiet.png");
-  }
-
   return (
     <footer className="flex flex-col justify-center items-center space-y-3 px-6 py-4">
       <InternalNavbar links={internalNavLinks} />
@@ -24,17 +13,11 @@ export default function Footer() {
           <ExternalNavbar links={externalNavLinks} />
         </div>
         <div className="flex flex-col justify-center items-center text-slate-500">
-          <p>Designed by Mark Satin</p>
-          <p>&copy; 2024</p>
+          <small>Designed by Mark Satin</small>
+          <small>&copy; 2024</small>
         </div>
         <div className="justify-self-end self-end">
-          <Image
-            src={audioIconPath}
-            alt="Black and white icon signalling audio status"
-            width={24}
-            height={24}
-            onClick={updateAudioIcon}
-          />
+          <AudioPlayer />
         </div>
       </div>
     </footer>
