a7a65bf6cef1e8d7102715437a27fa5c7a48d646
Mark Satin <msatin2009@gmail.com>
2024-04-20 13:19:54 -0400

    update photo; set width, height

--- app/entrance/page.tsx
+++ app/entrance/page.tsx
@@ -1,8 +1,8 @@
-import ImageReveal from "../ui/reveal";
+import ImageReveal from "../ui/image-reveal";
 import { getOnePhoto } from "../lib/actions";
 
 export default async function Page() {
-  const entrancePhoto = await getOnePhoto(57);
+  const entrancePhoto = await getOnePhoto(283);
 
-  return <div>{entrancePhoto && <ImageReveal image={entrancePhoto} />}</div>;
+  return <div className="w-full h-full">{entrancePhoto && <ImageReveal image={entrancePhoto} eraserSizeFactor={0.5} />}</div>;
 }
