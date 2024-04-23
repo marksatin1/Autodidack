81c9dcdba3342c5670be37b52ee879ac22630964
Mark Satin <msatin2009@gmail.com>
2024-04-23 18:02:31 -0400

    add disabled functionality

--- app/ui/forms/button.tsx
+++ app/ui/forms/button.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
 import { ImageType } from "../../lib/definitions";
 
-export default function Button({ name, icon }: { name: string; icon: ImageType }) {
+export default function Button({
+  name,
+  icon,
+  disabled,
+}: {
+  name: string;
+  icon: ImageType;
+  disabled: boolean;
+}) {
   return (
-    <div className="button-shadow px-5 py-2 flex justify-around items-center font-black text-2xl gap-3 rounded-full border-black border cursor-pointer hover:bg-slate-400 duration-500">
+    <div
+      className={`${
+        disabled ? "hover:cursor-help" : "cursor-pointer"
+      } button-shadow px-5 py-2 flex justify-around items-center font-black text-2xl gap-3 rounded-full border-black border hover:bg-slate-400 duration-500`}
+    >
       <p>{name}</p>
       <Image
         src={icon.path}
