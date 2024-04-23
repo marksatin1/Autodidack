bc47291e5b911fda91ce28f76a9e87253f5efeaf
Mark Satin <msatin2009@gmail.com>
2024-04-23 18:03:36 -0400

    add disabled functionality to unfinished pages

--- app/ui/layout/internal-navbar.tsx
+++ app/ui/layout/internal-navbar.tsx
@@ -9,13 +9,14 @@ export default function InternalNavbar({ links }: { links: NavLink[] }) {
     <nav className="w-full">
       <ul className="grid grid-cols-4 gap-32 items-center justify-center title-shadow">
         {links.map(l => {
+          const disabled = l.name === "Web" || l.name === "Store";
           return (
             <li key={l.id} className="text-3xl text-center tracking-widest">
               <Link
-                href={l.href}
+                href={disabled ? "" : l.href}
                 className={`${
                   pathname.slice(1) === l.name.toLowerCase() ? "link-selected" : "link"
-                }`}
+                } ${disabled && "line-through decoration-black decoration-wavy cursor-help"}`}
               >
                 <h2>{l.name}</h2>
               </Link>
