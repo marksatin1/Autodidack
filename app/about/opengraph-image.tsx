5b96893864de33eefd7e99819dfb3eb138d829d5
Mark Satin <msatin2009@gmail.com>
2024-04-23 10:29:32 -0400

    metadata updates/testing

---
+++ app/about/opengraph-image.tsx
@@ -0,0 +1,24 @@
+// import { ImageResponse } from "next/og";
+
+// export const alt = "Autodidack | About";
+// export const contentType = "image/png";
+// export const size = {
+//   width: 1200,
+//   height: 692,
+// };
+
+// export default function Image() {
+//   return (
+//     new ImageResponse(
+//       (
+//         <img
+//           src="https://autodidack.com/pages/about"
+//           width={size.width}
+//           height={size.height}
+//           alt={alt}
+//         />
+//       )
+//     ),
+//     { ...size }
+//   );
+// }
