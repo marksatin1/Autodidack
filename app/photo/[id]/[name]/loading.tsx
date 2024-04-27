ad45f23f8657b390acf7b2c488c16417b64ecac9
Mark Satin <msatin2009@gmail.com>
2024-04-26 22:19:31 -0400

    init commit

---
+++ app/photo/[id]/[name]/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingAnimation from "../../../ui/loading-animation";
+
+export default function Loading() {
+  return (
+    <section className="w-full h-full flex items-center justify-center">
+      <LoadingAnimation />
+    </section>
+  );
+}
