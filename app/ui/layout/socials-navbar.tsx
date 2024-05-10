6062bfe7bc5383f37fbbb24273c442c298f83126
Mark Satin <msatin2009@gmail.com>
2024-05-10 16:47:09 -0400

    update width, gap

--- app/ui/layout/socials-navbar.tsx
+++ app/ui/layout/socials-navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function SocialsNavbar({ links }: { links: NavLink[] }) {
   return (
     <nav className="h-fit">
-      <ul className="flex items-center justify-center gap-x-4 lg:gap-x-8">
+      <ul className="flex items-center justify-center gap-x-8">
         {links.map(l => {
           return (
             <li key={l.id} className="hover:scale-125 duration-700">
@@ -15,7 +15,7 @@ export default function SocialsNavbar({ links }: { links: NavLink[] }) {
                   alt={l.image!.description}
                   width={l.image!.width}
                   height={l.image!.height}
-                  className="w-6 md:w-[24px] md:h-[24px]"
+                  className="w-5 md:w-[24px] md:h-[24px]"
                 />
               </Link>
             </li>
