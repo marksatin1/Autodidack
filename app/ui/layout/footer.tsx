34726c0d409e1b44404c120a7be8b81e9ba0eede
Mark Satin <msatin2009@gmail.com>
2024-05-10 11:59:43 -0400

    add showMenu functionality

--- app/ui/layout/footer.tsx
+++ app/ui/layout/footer.tsx
@@ -2,22 +2,24 @@
 
 import { externalNavLinks, internalNavLinks } from "../../lib/data";
 import AudioPlayer from "./audio-player";
-import ExternalNavbar from "./external-navbar";
+import SocialsNavbar from "./socials-navbar";
 import InternalNavbar from "./internal-navbar";
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
 import { footerVariants } from "../../lib/animate-context";
+import Image from "next/image";
 
-// LEVER
-const animationDuration = 2000;
+const pathCheck = new RegExp("/[A-Za-z0-9]+/[0-9]+/[A-Za-z]+");
 
-export default function Footer() {
-  const pathCheck = new RegExp("/[A-Za-z0-9]+/[0-9]+/[A-Za-z]+");
+export default function Footer({ animationDuration }: { animationDuration: number }) {
   const pathname = usePathname();
   const currentPathsMatch = useMemo(() => pathCheck.test(pathname), [pathname]);
-  const [hideTrigger, setHideTrigger] = useState<boolean>(false);
 
+  const [hideTrigger, setHideTrigger] = useState<boolean>(false);
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+  // Footer hide/show animation control
   useEffect(() => {
     currentPathsMatch &&
       setTimeout(() => {
@@ -36,20 +38,26 @@ export default function Footer() {
         variants={footerVariants}
         className={`grow ${
           hideTrigger ? "hidden" : "block"
-        } flex flex-col justify-end items-center space-y-3 px-6 py-4`}
+        } flex flex-col justify-end items-center space-y-3 py-3`}
       >
-        <InternalNavbar links={internalNavLinks} />
-        <hr className="w-3/4 lg:w-1/4 border-slate-500" />
-        <div className="w-full grid grid-cols-3">
-          <div className="justify-self-start self-end">
-            <ExternalNavbar links={externalNavLinks} />
-          </div>
-          <div className="flex flex-col justify-center items-center text-center text-slate-500">
+        {showMenu && <InternalNavbar links={internalNavLinks} />}
+        <div className="w-full flex flex-col justify-center items-center gap-3">
+          <SocialsNavbar links={externalNavLinks} />
+          <hr className="w-3/4 lg:w-1/4 border-slate-500" />
+          <article className="flex flex-col justify-center items-center text-center text-slate-500">
             <small>Designed by Mark Satin</small>
             <small>&copy; 2024</small>
-          </div>
+          </article>
           <div className="justify-self-end self-end">
             <AudioPlayer />
+            <Image
+              src="/icons/svg/HamburgerRight.svg"
+              width={64}
+              height={64}
+              alt="Hamburger menu"
+              onClick={() => setShowMenu(!showMenu)}
+              className="w-8"
+            />
           </div>
         </div>
       </motion.footer>
