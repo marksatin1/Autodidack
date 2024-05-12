b536ec7ed4c036424c543074c8bc139b6f6bdb0c
Mark Satin <msatin2009@gmail.com>
2024-05-12 11:51:36 -0400

    update responsive styles

--- app/ui/forms/button.tsx
+++ app/ui/forms/button.tsx
@@ -6,18 +6,20 @@ export default function Button({ id, type, ariaLabel, icon, disabled }: FormButt
     <button
       id={id}
       type={type}
-      aria-label={ariaLabel}
+      aria-label={ariaLabel}    
       className={`${
         disabled ? "hover:cursor-help" : "cursor-pointer"
-      } button-shadow px-3 py-2 sm:px-5 sm:py-2 flex justify-around items-center font-black text-xs sm:text-2xl gap-3 rounded-full border-black border hover:bg-slate-400 hover:text-accent-dark duration-500`}
+      } button-shadow px-3 py-1 sm:px-5 sm:py-2 flex justify-around items-center gap-2 rounded-full border-black border hover:bg-slate-200 duration-500`}
     >
-      <p>{id}</p>
+      <p className="font-black text-xs sm:text-lg lg:text-xl hover:text-accent-dark duration-500">
+        {id}
+      </p>
       <Image
         src={icon.path}
         width={icon.width}
         height={icon.height}
         alt={icon.description}
-        className="w-[16px] h-[16px] sm:w-[32px] sm:h-[32px]"
+        className="w-[16px] h-[16px] sm:w-[24px] sm:h-[24px] md:w-[32px] md:h-[32px]"
       />
     </button>
   );
