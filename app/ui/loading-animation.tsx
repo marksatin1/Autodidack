ed8a90fcbb80783008021d81bfe51a7b09a7226f
Mark Satin <msatin2009@gmail.com>
2024-05-12 17:25:39 -0400

    fetch flags remotely and pass to auto-carousel

--- app/ui/loading-animation.tsx
+++ app/ui/loading-animation.tsx
@@ -2,13 +2,24 @@
 
 import { motion } from "framer-motion";
 import AutoCarousel from "./components/auto-carousel";
-import { flags } from "../lib/data";
 import { loadingVariants } from "../lib/animate-context";
+import { useEffect, useState } from "react";
+import { getImagesInSequentialOrder } from "../lib/actions";
+import { ImageType } from "../lib/definitions";
 
 export default function LoadingAnimation() {
+  const [loadingImages, setLoadingImages] = useState<ImageType[] | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const flags = await getImagesInSequentialOrder(11);
+      setLoadingImages(flags);
+    })();
+  }, []);
+
   return (
     <motion.div animate="grow" transition={{ duration: 20 }} variants={loadingVariants}>
-      {<AutoCarousel images={flags} fadeInterval={150} variants="" />}
+      {loadingImages && <AutoCarousel images={loadingImages} fadeInterval={150} variants="" />}
     </motion.div>
   );
 }
