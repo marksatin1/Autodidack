d6930772aad7ccfb5e147cef99049d8a082886d5
Mark Satin <msatin2009@gmail.com>
2024-05-24 12:44:48 -0400

    init commit

---
+++ app/ui/components/image-spin.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect } from "react";
+import { ImageType } from "../../lib/definitions";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { tiltSpinVariants } from "../../lib/motion-variants";
+import { redirect } from "next/navigation";
+
+export default function ImageSpin({ image }: { image: ImageType }) {
+  function handleRedirect() {
+    window.location.href = "/";
+  }
+
+  return (
+    <motion.div
+      initial="initial"
+      animate="end"
+      transition={{
+        ease: "easeIn",
+        duration: 10,
+        delay: 0.5,
+      }}
+      variants={tiltSpinVariants}
+      onAnimationComplete={handleRedirect}
+      className="h-full flex justify-center items-center"
+    >
+      <Image
+        src={image.path}
+        width={image.width}
+        height={image.height}
+        alt={image.description}
+        priority
+        className="w-auto max-h-[600px]"
+      />
+    </motion.div>
+  );
+}
