954b7712cb75ff5f39505a237a4cd030bc71d868
Mark Satin <msatin2009@gmail.com>
2024-08-28 15:26:33 -0400

    remove arrows, AnimatePresence, footer animation

--- app/ui/components/carousel.tsx
+++ app/ui/components/carousel.tsx
@@ -5,7 +5,6 @@ import { ImageType } from "../../lib/definitions";
 import Image from "next/image";
 import { icons } from "../../lib/data";
 import { AnimatePresence, motion } from "framer-motion";
-import { carouselVariants } from "../../lib/motion-variants";
 
 export default function Carousel({ slides }: { slides: ImageType[] }) {
   const [currentIdx, setCurrentIdx] = useState<number>(0);
@@ -44,62 +43,31 @@ export default function Carousel({ slides }: { slides: ImageType[] }) {
   }
 
   return (
-    <AnimatePresence mode="wait">
-      <motion.article
-        tabIndex={0} // makes <section> focusable
-        ref={sectionRef}
-        onKeyDown={handleKeyDown}
-        onTouchStart={(e: any) => {
-          setTouchStartX(e.touches[0].clientX);
-        }}
-        onTouchMove={(e: any) => setTouchEndX(e.touches[0].clientX)}
-        onTouchEnd={handleSwipe}
-        layout
-        initial="initial"
-        animate="animate"
-        transition={{ duration: 2.3 }}
-        variants={carouselVariants}
-        className="w-full h-full outline-none grid grid-rows-[1fr_auto]"
-      >
-        <section className="relative w-full h-[77%] sm:h-full">
-          {slides.map((p: ImageType, i: number) => (
-            <Image
-              key={p.id}
-              src={p.path}
-              width={p.width}
-              height={p.height}
-              alt={p.description}
-              className={`absolute w-full h-full object-contain ${
-                i === currentIdx ? "opacity-100" : "opacity-0"
+    <motion.article
+      tabIndex={0} // makes <section> focusable
+      ref={sectionRef}
+      onKeyDown={handleKeyDown}
+      onTouchStart={(e: any) => {
+        setTouchStartX(e.touches[0].clientX);
+      }}
+      onTouchMove={(e: any) => setTouchEndX(e.touches[0].clientX)}
+      onTouchEnd={handleSwipe}
+      layout
+      className="w-full h-full outline-none grid grid-rows-[1fr_auto]"
+    >
+      <section className="relative w-full h-[77%] sm:h-full">
+        {slides.map((p: ImageType, i: number) => (
+          <Image
+            key={p.id}
+            src={p.url}
+            width={800}
+            height={800}
+            alt={p.description}
+            className={`absolute w-full h-full object-contain ${i === currentIdx ? "opacity-100" : "opacity-0"
               } duration-[2s]`}
-            />
-          ))}
-        </section>
-        <section className="hidden sm:flex sm:justify-center sm:my-4">
-          <Image
-            src={icons.arrowLeftThin.path}
-            width={icons.arrowLeftThin.width}
-            height={icons.arrowLeftThin.height}
-            alt={icons.arrowLeftThin.description}
-            onClick={handlePrevImage}
-            role="button"
-            aria-label="previous photo button"
-            tabIndex={0}
-            className="w-[48px] cursor-pointer"
           />
-          <Image
-            src={icons.arrowRightThin.path}
-            width={icons.arrowRightThin.width}
-            height={icons.arrowRightThin.height}
-            alt={icons.arrowRightThin.description}
-            onClick={handleNextImage}
-            role="button"
-            aria-label="next photo button"
-            tabIndex={0}
-            className="w-[48px] cursor-pointer"
-          />
-        </section>
-      </motion.article>
-    </AnimatePresence>
+        ))}
+      </section>
+    </motion.article>
   );
 }
