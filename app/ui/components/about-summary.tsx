7038de6582b08d20fb4897f2096453b6245daf57
Mark Satin <msatin2009@gmail.com>
2024-08-30 15:46:16 -0400

    updated content

--- app/ui/components/about-summary.tsx
+++ app/ui/components/about-summary.tsx
@@ -1,160 +1,128 @@
 export default function AboutSummary() {
   return (
-    <div className="sm:px-5 sm:py-2 text-sm sm:text-lg text-justify text-white text-content-shadow lg:text-xl">
+    <div className="sm:px-5 sm:py-2 text-xs sm:text-lg text-justify text-white text-content-shadow lg:text-xl">
       <aside>
         <p>
-          Autodidack is a portmanteau of “autodidact” - a self-taught person - and “Adirondacks” -
-          the name given to the 5000 square miles of lush mountain range in upstate New York.
+          <i>Autodidack</i> is a portmanteau of “autodidact” - a self-taught person - and “Adirondacks” -
+          the name given to 5000 square miles of lush mountain range in upstate New York .......
         </p>
       </aside>
       <br />
       <br />
       <p>
-        When the pandemic hit NYC in early 2020 I had just rediscovered my passion for 35mm film. I
+        When the pandemic hit NYC in early 2020 I had just rediscovered my love of 35mm film. I
         hadn&#39;t photographed much of anything in the previous five years, but a new job renting
-        film equipment to local productions had whet my appetite again. Armed with a couple of
-        cameras and a glut of free time I began crisscrossing the city&#39;s deserted neighborhoods
-        simply as a way to get out of the house and distract myself from the bleakness of daily life
-        under Covid. This was a period of great experimentation that led to moments of real joy. I
-        made it my mission to expand my boundaries as much as possible: I shot people I knew and
-        didn&#39;t know, random animals, empty places, nature, the city, anything. I shot any film
-        stock I could get my hands on: black and white, color, Kodak, Fuji, slides, Ilford. I
-        learned how to use diffusion filters and a polarizer. I set my apertures wide open and
-        stopped them all the way down.
+        film equipment to local productions had whet my appetite again. When Covid forced us to shutter our doors, I poured my energy into my photography, grabbing my cameras
+        and crisscrossing deserted neighborhoods,
+        mostly as a way to distract myself from the bleakness of daily life.</p>
+      <br />
+      <br />
+      <p>This turned out to be a period of great experimentation that led to moments of real joy. I put myself through a crash course in photography, and made it my mission to expand my creative boundaries as much as possible, capturing initmate portraits of friends and strangers as well as cityscapes and street scenes subdued by a lack of traffic.
+        I shot any film stock I could get my hands on. I started using filters daily, eventually learning to balance my exposures with a polarizer and ND gradients. I started using my tripod more, stopping way down on the aperture and exposing for longer periods of time. Photography was becoming less of a point-and-shoot activity and more and more
+        an opportunity to merge my vision of things with the objects and spaces around me.
       </p>
       <br />
       <br />
       <p>
-        When I ventured out I always expected to see someone around the next corner but I rarely
-        did. And when I did, they seemed to be pulled quickly past me by the spirit of a hurried
-        traveler. Some neighborhoods were so dead I had to keep reminding myself that millions of
-        New Yorkers were still all around me, locked up a few stories higher inside their
-        apartments.
+        Despite my productivity it was still strange to venture out into the stillness. My brain expected to constantly be surrounded by people,
+        but nearly every time I rounded a corner I was met with nothing. Some neighborhoods were so dead I had to remind myself that millions of
+        New Yorkers still lived here, locked up inside their apartments. As time pressed on I shifted my focus away from the lack of people and sank my teeth into the
+        spaces I passed through, suddenly in tune with the buildings and neighborhoods in a way I had never been before.
       </p>
       <br />
       <br />
       <p>
-        Two months into this mess, George Floyd was murdered. For the second time in two months the
-        social reality changed drastically overnight. Suddenly everyone had a reason to go outside
-        again: people were fed up with being trapped, not just within their apartments, but within a
-        system that continued to empower the empowered and disadvantage the disadvantaged. Not even
-        a global pandemic could stop police from murdering black people, and now no one could ignore
-        that fact.
+        Two months into the pandemic George Floyd was murdered by a cop, and all those people burst out into the streets.
+        For the second time in two months the
+        social reality changed overnight. Breonna Taylor's name was quickly reinvoked, and justice was demanded for dozens of black Americans murdered by police.
+        It felt as if people were fed up with being trapped - not just within their apartments but within a
+        system that continued to empower the empowered and disadvantage the disadvantaged. Police brutality had persisted through a global pandemic and billions
+        of people sat idly by in their homes. Now millions would march.
       </p>
       <br />
       <br />
       <p>
-        The Roman philosopher Seneca said, “Luck is when preparation meets opportunity”. Since
-        starting my artistic journey I had shot 38 rolls of film, or more than 1300 photos. Walking
-        and biking around the city had become my daily exercise: capturing the day-to-day through my
-        lens tricked me into believing I was dealing with my own part of our collective grief. In
-        reality that processing would come much later, in layers that washed over me each of the
-        thousands of times I looked at my developed photos.
+        I started photographing the protests immediately. I went to document history as an observer but quickly became a participant.
+        Just by being present at rallies and by
+        engaging with the community through my camera, I learned a lot about a section of society I had previoulsy so easily ignored.
+        I listened to firsthand
+        accounts of police brutality and heard from local activists dedicating their lives to
+        social equality and legal protection for all citizens. I learned how journalists chose their stories and
+        photographers shot them. I saw how media conglomerates wielded their influence and shaped the global narrative while grassroots community activism
+        took root over social media and worked to present a real challenge to those companies.
       </p>
       <br />
+      <hr className="w-3/4 m-auto my-2 md:my-3 md:border-2 rounded" />
       <br />
       <p>
-        I started photographing the protests immediately. Just by being present at rallies, and by
-        engaging with the community through my camera, I learned so much: I listened to firsthand
-        accounts of police brutality and heard from local activists dedicating their lives to true
-        social equality and legal protection; I learned how journalists chooses their stories and
-        photographers shoot them; I learned about the advantages of big media companies and the
-        disadvantages of promoting community activism over social media.
-      </p>
-      <br />
-      <br />
-      <p>
-        Each week I would get back the previous drop&#39;s photos from the labs. As I reviewed the
-        scans I could feel the anger and fear and resilience on people&#39;s faces. It charged the
-        spaces I shot with a palpable intensity, something that struck a chord in me beyond the
-        scope of current events. It also made me realize just how much had changed in three months:
+        Since starting my artistic journey I had shot 38 rolls of film - more than 1300 photos. Walking
+        and biking around the city had become my physical exercise, capturing day-to-day life my mental and emotional exercise.
+        Each week I would get back the previous drop&#39;s photos from the lab. As I scanned images
+        I could feel the anger and fear and resilience on people&#39;s faces. It charged the
+        spaces I shot with a palpable intensity, something that struck a chord in me and didn&#39;t leave. It also made me realize just how much had changed in three months:
         I had photos of Broadway boarded up and empty; of masked shopkeepers behind hanging sheets
         of plastic; of looters in SoHo and protestors in Greenwich Village; of marches across
-        Houston Street and over FDR Drive. In June alone I shot 14 rolls.
+        Houston Street and over FDR Drive. In June alone I shot 14 more rolls.
       </p>
       <br />
       <br />
       <p>
-        In the middle of the summer I took a trip to Bethany Beach, DE and then back home to
+        In the middle of the summer I took a trip to Bethany Beach, Delaware and then back home to
         Maryland. I wanted to see how people in the suburbs were handling the lockdown as Covid
-        cases spread outwards from NYC. From there I took a road trip across the country, fulfilling
-        a bucket list item and capturing irrefutable evidence of a nation left behind by its
-        leaders. Another dimension to the fear and anger began to reveal itself. This was an
-        election year - the one that would determine whether Donald Trump would get a second term -
-        and I made my way across eighteen states, talking with people, photographing them and their
-        highways and homes, their state parks and our national treasures.
+        cases spread outwards from NYC. It was strange to weather the storm with my family after living in something like solitary confinement for so long,
+        and witnessing the pandemic from this angle made me realize that I had only experienced our historical turbulence from a couple of limited perspectives.
+        On top of everything else, this was an
+        election year - one that would determine if Donald Trump got a second term - and I wanted to know how oridinary Americans were handling the mass unrest.
+        A few weeks before Election Day I left home and embarked on a road trip across the country, checking off a
+        bucket list item and capturing irrefutable evidence of a nation left behind by its
+        leaders. I made my way across eighteen states, talking with people along the way and photographing both treasured national parks and
+        the forgotten towns between them.
       </p>
       <br />
       <br />
       <p>
-        Traveling on the road alone gave me a lot of time to think. It was becoming clear to me that
-        I&#39;d been feeling restless for a long time. I had tried many different jobs in my
-        twenties but hadn&#39;t found a career I cared about for long enough. I loved New York but
-        also felt trapped by it. But now my own anger, my own sense of feeling stuck, had been given
-        national appreciation, and I couldn&#39;t ignore it any longer.
+        Traveling on the road alone gives you a lot of time to think. In that environment it became clear to me that the restlessness
+        I had been documenting was not just national, but my own. I had tried many different jobs in my
+        twenties, and liked aspects of all of them, but never quite felt like I had found my niche. Returning home after five weeks on the road, I was intent on finding something that would engage and excite me as
+        much as the last eight months had. Journalistic photography had changed my life but I didn&#39;t just want to report on
+        current events; I wanted to interact with them and produce art from those interactions. And I wanted that art to build towards something cohesive - but what?
       </p>
       <br />
       <br />
       <p>
-        Upon returning home I was intent on finding something that would engage and excite me as
-        much as the last eight months had. Journalistic photography had changed my life but I knew
-        that lifestyle was not the right fit for me full-time. Besides, I didn&#39;t just want to
-        report on current events; I wanted to make art that could stand apart from the day-to-day. I
-        also wanted to build something; the question was what to build. I&#39;ve always loved
-        learning, even for its own sake, but what would I learn next?
-      </p>
-      <br />
-      <br />
-      <p>
-        The answer ended up presenting itself in the most unsuspecting way possible. One day my
-        uncle told me in passing that Harvard offered a free online computer science course called
-        CS50x. He saw CS as the future and had heard good things about this class. I enrolled and by
+        The answer presented itself in the most unsuspecting way possible. One day my
+        uncle told me in passing about a free online computer science course called
+        CS50x. He saw software engineering as the future of all industries and had heard good things about this class. I enrolled and by
         the end of the first lecture I was hooked. Over the course of five months I realized that I
         actually had an engineering brain, but that I hadn&#39;t had the opportunity to use it as
-        much as I would have liked. Despite not possessing this understanding previously, it
-        actually made complete sense to me once I gave it some thought. After all, as a Producer and
-        Lighting Designer on short films and commercials, I had to engineer solutions to complex
+        much as I would have liked. In a way, this made sense to me. After all, as a Producer and
+        Lighting Designer on short films and commercials I had had to engineer solutions to complex
         logistical problems all the time.
       </p>
       <br />
       <br />
       <p>
-        It was at this time that I began to see the possibilities of uniting visual storytelling
-        with well-thought-out user experiences on the web. Navigating to a website didn&#39;t just
-        have to be a utilitarian concern: the experience of getting new information from a web page
-        could combine aspects of the story of a great television show, the user interactivity of
-        your favorite video game, and the educational environment of an on-demand virtual classroom.
-      </p>
-      <br />
-      <br />
-      <p>
-        Since getting my first computer at age nine I&#39;ve had a real fondness for the novelty of
-        the web. While I love how fast the internet has become and how robust web apps are, I do
-        miss the wild west ethos of early online spaces. I miss not knowing what&#39;s at the end of
-        the next link. I miss knowing that any piece of information presented in any myriad number
-        of ways could be around that corner.
-      </p>
-      <br />
-      <br />
-      <p>
-        As humans we have a big spiritual need for art in our lives, to continuously see creativity
-        expressed around us in new and interesting ways. Frankly, we become unhappy when we
+        In this course I began to see the posibilities of connecting my art to robust user experiences on the web.
+        The internet is utilitarian, but it&#39;s also interactive and engaging, and navigating to a website didn&#39;t just
+        have to be a utilitarian experience: retrieving information from a web page
+        could combine the story of a great television show, the UI of
+        a great video game, and the educational environment of an on-demand virtual classroom.
+        As humans we have a spiritual need for art in our lives, to continuously see creativity
+        expressed around us in new and interesting ways. I believe we become unhappy when we
         don&#39;t have access to art: it excites us even when we&#39;re incapable of finding words
-        to describe that excitement. My engineering and artistic practice is based in combining the
-        creativity and seeming entropy of Web 1.0 with the agile practices of the modern Software
-        Development Life Cycle.
+        to describe that excitement.
       </p>
       <br />
       <br />
       <p>
         When it became clear that I was going to lasso my varying pursuits into one portfolio I
-        began brainstorming words and phrases that could encapsulate exactly what it is I have to
-        offer. I reflected on my love of learning in and out of the classroom, and of trying new
+        began brainstorming words and phrases that could encapsulate my experiences over this year. I reflected on my love of learning in and out of the classroom, and of trying new
         things and becoming empowered by my pursuit of new experiences. Returning from a trip to the
         Adirondacks one summer I eagerly developed my film and was moved deeply by the ethereal
         beauty of a waterfall I had photographed on Mt. Algonquin. The memory of this moment lodged
-        itself in my brain, and a new portmanteau was born: Autodidack, the union of a
-        self-motivated learner seeking solace, knowledge, and epiphany in nature.
+        itself in my brain, and a new portmanteau was born: Autodidack, a
+        self-motivated learner seeking solace, knowledge, and epiphany in nature. In my artistic and engineering practice
+        I hope to impart this same emotion in you, the viewer.
       </p>
     </div>
   );
